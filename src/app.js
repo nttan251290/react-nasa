1c90807074c3a34e4bd51743409f0189606d106c
nttan251290 <nttan251290@gmail.com>
2020-03-04 07:57:52 +0700

    - First commit.

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+const template = <p>Hello fefew react 333</p>;
+
+ReactDOM.render(template, document.getElementById('root'));
